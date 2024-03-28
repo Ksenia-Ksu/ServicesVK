adf834c4189d9acc97fc013f33f8300a7b605bc3
Ksenia Kobak <79209115+Ksenia-Ksu@users.noreply.github.com>
2024-03-28 14:40:25 +0300

    Initial Commit

---
+++ ServicesVK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ServicesVK
+//
+//  Created by Ксения Кобак on 28.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
